b677ee056fd24a833d881f631d79e58a24ee60cb
Eldad Zack <eldad.zack@egym.com>
2022-01-11 15:12:16 +0100

    red

--- src/theme.rs
+++ src/theme.rs
@@ -31,7 +31,7 @@ impl FlatCwdScheme for SimpleTheme {
 
 impl ExitCodeScheme for SimpleTheme {
     const EXIT_CODE_BG: Color = Color(0);
-    const EXIT_CODE_FG: Color = Color(161);
+    const EXIT_CODE_FG: Color = Color(160);
 }
 
 impl UserScheme for SimpleTheme {
@@ -67,7 +67,7 @@ impl GitScheme for SimpleTheme {
     const GIT_NOTSTAGED_FG: Color = Color(15);
     const GIT_REPO_CLEAN_BG: Color = Color(148);
     const GIT_REPO_CLEAN_FG: Color = Color(0);
-    const GIT_REPO_DIRTY_BG: Color = Color(161);
+    const GIT_REPO_DIRTY_BG: Color = Color(160);
     const GIT_REPO_DIRTY_FG: Color = Color(15);
     const GIT_STAGED_BG: Color = Color(22);
     const GIT_STAGED_FG: Color = Color(15);
