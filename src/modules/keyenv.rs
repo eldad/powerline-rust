bf0f2df33032094047b3432689a575a81d3021dc
Eldad Zack <eldad.zack@egym.com>
2022-12-29 15:53:58 +0100

    join with spaces in between

--- src/modules/keyenv.rs
+++ src/modules/keyenv.rs
@@ -24,8 +24,13 @@ const KEY_ENV_NAME: &str = "PROMPT_KEYS";
 impl<S: KeyEnvScheme> Module for KeyEnv<S> {
     fn append_segments(&mut self, powerline: &mut Powerline) {
         if let Some(keys) = env::var_os(KEY_ENV_NAME) {
-            let keys = keys.to_string_lossy();
-            powerline.add_segment(format!("{}\u{2026}\u{2049}", keys), Style::simple(S::KEY_ENV_FG, S::KEY_ENV_BG))
+            let keys: String = keys
+                .to_string_lossy()
+                .split_whitespace()
+                .map(|s| format!("\u{1F511}{}", s))
+                .collect::<Vec<String>>()
+                .join(" ");
+            powerline.add_segment(keys, Style::simple(S::KEY_ENV_FG, S::KEY_ENV_BG))
         }
     }
 }
