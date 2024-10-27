0a3009037ef08537e1ddd34eac9b67180592d2e8
Eldad Zack <eldad@fogrefinery.com>
2024-10-27 14:00:18 +0100

    adjust git icons

--- src/modules/git.rs
+++ src/modules/git.rs
@@ -110,7 +110,7 @@ fn duration_string(duration: &Duration) -> String {
     if weeks > 0 || days > 0 || hours > 0 {
         timestr += &format!("{}h", hours);
     }
-    if weeks > 0 || days > 0 || hours > 0 || minutes > 0{
+    if weeks > 0 || days > 0 || hours > 0 || minutes > 0 {
         timestr += &format!("{}m", minutes);
     }
 
@@ -137,31 +137,30 @@ impl<S: GitScheme> Module for Git<S> {
 
         powerline.add_segment(stats.branch_name, Style::simple(branch_fg, branch_bg));
 
-        let mut add_elem = |count: u32, symbol, fg, bg| match count.cmp(&1) {
+        let mut add_elem = |count: u32, symbol: &str, fg, bg| match count.cmp(&1) {
             Ordering::Equal => powerline.add_segment(symbol, Style::simple(fg, bg)),
             Ordering::Greater => powerline.add_segment(format!("{count}{symbol}"), Style::simple(fg, bg)),
             Ordering::Less => (),
         };
 
-        add_elem(stats.ahead, '\u{2B06}', S::GIT_AHEAD_FG, S::GIT_AHEAD_BG);
-        add_elem(stats.behind, '\u{2B07}', S::GIT_BEHIND_FG, S::GIT_BEHIND_BG);
-        add_elem(stats.staged, '\u{2714}', S::GIT_STAGED_FG, S::GIT_STAGED_BG);
-        add_elem(stats.non_staged, '\u{270E}', S::GIT_NOTSTAGED_FG, S::GIT_NOTSTAGED_BG);
-        add_elem(stats.untracked, '\u{2753}', S::GIT_UNTRACKED_FG, S::GIT_UNTRACKED_BG);
-        add_elem(stats.conflicted, '\u{273C}', S::GIT_CONFLICTED_FG, S::GIT_CONFLICTED_BG);
+        add_elem(stats.ahead, "🔺", S::GIT_AHEAD_FG, S::GIT_AHEAD_BG);
+        add_elem(stats.behind, "🔻", S::GIT_BEHIND_FG, S::GIT_BEHIND_BG);
+        add_elem(stats.staged, "✅", S::GIT_STAGED_FG, S::GIT_STAGED_BG);
+        add_elem(stats.non_staged, "✏️", S::GIT_NOTSTAGED_FG, S::GIT_NOTSTAGED_BG);
+        add_elem(stats.untracked, "❓", S::GIT_UNTRACKED_FG, S::GIT_UNTRACKED_BG);
+        add_elem(stats.conflicted, "✳️", S::GIT_CONFLICTED_FG, S::GIT_CONFLICTED_BG);
 
         if let Some(fetch_head_age) = stats.fetch_head_age {
-
             if fetch_head_age.as_secs() > 3600 {
                 let symbol = '\u{23F0}';
 
-                let fg = if fetch_head_age.as_secs() > 12 * 3600 {
-                    S::GIT_FETCH_AGE_FG
-                } else {
-                    S::GIT_FETCH_AGE_STALE_FG
-                };
+                let fg =
+                    if fetch_head_age.as_secs() > 12 * 3600 { S::GIT_FETCH_AGE_FG } else { S::GIT_FETCH_AGE_STALE_FG };
 
-                powerline.add_segment(format!("{}{}", duration_string(&fetch_head_age), symbol), Style::simple(fg, S::GIT_FETCH_AGE_BG));
+                powerline.add_segment(
+                    format!("{}{}", duration_string(&fetch_head_age), symbol),
+                    Style::simple(fg, S::GIT_FETCH_AGE_BG),
+                );
             }
         }
     }
